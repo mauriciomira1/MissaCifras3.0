5680e2fe76099e04ead8b7c1d9905f7d590724b7
Maurício Miranda <mauriciomirandagomes@gmail.com>
2023-12-19 23:42:51 -0300

    feat: submenu

--- src/components/Submenu/ItemSubmenu.tsx
+++ src/components/Submenu/ItemSubmenu.tsx
@@ -8,12 +8,13 @@ interface itemsProps {
 const ItemSubmenu = (props: itemsProps) => {
   return (
     <div>
-      <Link
-        href={`/${props.slug}`}
-        className="w-36 h-8 bg-white border rounded border-gray-200 font-bold text-xs font-text text-primaryColor
+      <Link href={`/${props.slug}`}>
+        <button
+          className="w-36 h-8 bg-white border rounded border-gray-200 font-bold text-xs font-text text-primaryColor
       hover:bg-secondaryColor hover:text-white duration-100 active:bg-primaryColor"
-      >
-        {props.name}
+        >
+          {props.name}
+        </button>
       </Link>
     </div>
   );
