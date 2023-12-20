3a0b12bd8113d0a590d2c25629ebc589b9673e14
Maurício Miranda <mauriciomirandagomes@gmail.com>
2023-12-20 12:26:10 -0300

    feat: add auth schema

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,16 @@
-import { PrismaClient } from "@prisma/client";
-import { AuthOptions } from "next-auth";
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 
-export const authOptions: AuthOptions = {
+const prisma = new PrismaClient();
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-};
+});
